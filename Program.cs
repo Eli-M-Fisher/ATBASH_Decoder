aacb4f62ada36cf4e1d26b18552dd96c71d8cc91
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-05-14 15:11:09 +0300

    Add main structure to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ATBASH_Decoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== ATBASH Decoder System ===");
+
+            // Example encrypted message for initial testing
+            string encryptedMessage = "Gsv xlwv gl zokszyvg ziv ulc...";
+
+            // Future integration with team modules:
+            // string decrypted = Decryption.Decrypt(encryptedMessage);
+            // string cleanText = CleanText.Clean(decrypted);
+            // List<string> foundWords = WordFinder.Find(cleanText, Constants.SuspiciousWords);
+            // string finalMessage = WordCounter.Analyze(cleanText, foundWords);
+
+            // Console.WriteLine(finalMessage);
+        }
+    }
+}
